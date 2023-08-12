192fa0026b08952bc9e0536c5db2e37fa6df6323
Asad Ahmadi <asad.ahmadi.bm@gmail.com>
2023-08-12 09:19:31 +0330

    Update Program.cs

--- SanaServiceTest2/Program.cs
+++ SanaServiceTest2/Program.cs
@@ -8,7 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IMemoryCache,MemoryCache>();
+//builder.Services.AddSingleton<IMemoryCache,MemoryCache>();
+builder.Services.AddMemoryCache();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
